737a1ec51407cad03d0db96bae9f6c52e095302c
SAN_LEN_P14S\sandeep <sandeep.spatil@gmail.com>
2022-11-22 16:24:54 +0100

    Added Contact Us form

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import home, about_us
+from .views import home, about_us, contact_us, thank_you_contact_us
 
 app_name = 'main'
 
 urlpatterns = [
     path('', home, name='home'),
     path('about-us', about_us, name='about_us'),
+    path('contact-us', contact_us, name='contact_us'),
+    path('thank-you-contact-us', thank_you_contact_us, name='thank_you_contact_us'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from main.forms import ContactForm
 
 
 # Create your views here.
@@ -15,3 +19,30 @@ def about_us(request):
         'title': 'About Us'
     }
     return render(request, 'pages/about_us.html', context)
+
+
+def contact_us(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+
+            print(subject, message, sender, cc_myself)
+
+            return HttpResponseRedirect(reverse('main:thank_you_contact_us'))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'pages/contact_us.html', {'form': form})
+
+
+def thank_you_contact_us(request):
+    return render(request, 'pages/thank_you_contact_us.html')
