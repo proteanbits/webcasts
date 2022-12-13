32dcfa5b83b2f1fa4c75246b5ae2a142a50dced7
SAN_LEN_P14S\sandeep <sandeep.spatil@gmail.com>
2022-12-13 15:02:58 +0100

    Fix a typo and a css padding issue

--- main/urls.py
+++ main/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', profile, name='profile'),
-    path('profile/update/$', ProfileUpdate.as_view(), name='profile_update'),
+    path('profile/update/', ProfileUpdate.as_view(), name='profile_update'),
 ]
